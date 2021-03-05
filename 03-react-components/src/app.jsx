7fcf24d6274f7405b75b7311e50b5d208f633361
Klaus Eckelt <Klaus.Eckelt@jku.at>
2021-03-05 14:57:55 +0100

    03 initial setup

---
+++ 03-react-components/src/app.jsx
@@ -0,0 +1,15 @@
+// react element:
+const reactElement = React.createElement(
+  'p', //create a paragraph
+  {},
+  `I'm a react element!`
+);
+
+// react component:
+const jsxElement = <p id="jsx-elem">I am still a React component.</p> // will be translated to a create element
+
+
+ReactDOM.render(
+  jsxElement, // use reactElement or jsxElement (needs rebuild if changed)
+  document.querySelector('#react-app')
+)
